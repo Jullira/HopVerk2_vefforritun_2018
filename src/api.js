f6271116ebc46265b5884a6da0abb089edbe65c8
sakkattack <isakkolbeins@gmail.com>
2018-04-20 01:43:27 +0000

    login hlutinn sem ég var með,,,, fullt af merge brasi örugglega

--- src/api.js
+++ src/api.js
@@ -3,7 +3,7 @@ const baseurl = process.env.REACT_APP_SERVICE_URL;
 
 async function get(endpoint) {
   const token = window.localStorage.getItem('token');
-
+  console.log(token);
   const url = `${baseurl}${endpoint}`;
 
   const options = {
@@ -15,8 +15,38 @@ async function get(endpoint) {
     options.headers['Authorization'] = `Bearer ${token}`;
   }
   
-  /* todo framkvæma get */
   const response = await fetch(url, options);
+  return response.json();
+}
+
+
+async function login(username, password) {
+  //const token = window.localStorage.getItem('token');
+
+  const url = `${baseurl}login`;
+
+  const settings = {
+    headers: {
+      'Content-type': 'application/json'
+    },
+    data: {
+      username,
+      password
+    },
+    method: 'POST',
+    "async": true,
+    "crossDomain": true
+  }
+   
+  /*
+  if (token) {
+    options.headers['Authorization'] = `Bearer ${token}`;
+  }*/
+  
+  const response = await fetch(url, settings);
+  
+
+  console.log(response);
   return response.json();
 }
 
@@ -24,4 +54,5 @@ async function get(endpoint) {
 
 export default {
   get,
+  login,
 };
--- src/routes/login/Login.js
+++ src/routes/login/Login.js
@@ -3,19 +3,59 @@ import { connect } from 'react-redux';
 import { NavLink } from 'react-router-dom';
 
 /* todo sækja actions frá ./actions */
-
+import api from '../../api';
+import Button from '../../components/button';
 import './Login.css';
+import { loginUser, logoutUser } from '../../actions/auth';
+
 
 class Login extends Component {
+  state = { username:'', password:  '' }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    const { dispatch } = this.props;
+    dispatch(loginUser(this.state.username, this.state.password));
+  }
+
+  handleInputChange = (e) => {
+    const { name, value } = e.target;
+    console.log( name, " -- ", value);
+    if (name) {
+      this.setState({ [name]: value });
+    }
+  }
+
+  handleLogout = (e) => {
+    const { dispatch } = this.props;
+    dispatch(logoutUser());
+  }
 
   render() {
+    const { username, password } = this.state;
+    const { isFetching, isAuthenticated, message } = this.props;
+
+    if (isFetching) {
+      return (
+        <p>Skrái inn <em>{username}</em>...</p>
+      );
+    }
+    
     return (
+        
       <div className = "login-page">
-        <form className = "login-form">
-          <div className="login-form-container">
-            <input type="text" placeholder="Notendanafn" name="name" required />
-            <input type="password" placeholder="Lykilorð" name="psw" required />
-            <button type="submit" className = "login-button">Skrá inn</button>
+      {message && (<p>{message}</p>) }
+        <form className = "login-form" onSubmit={this.handleSubmit}>
+          <div className="login-form-container">          
+            <div>
+              <label htmlFor="username">Notendanafn:</label>
+              <input id="username" type="text" name="username" value={username} required onChange={this.handleInputChange} />
+            </div>
+            <div>
+              <label htmlFor="password">Lykilorð:</label>
+              <input id="password" type="password" name="password" value={password} required onChange={this.handleInputChange} />
+            </div>
+            <Button disabled={isFetching}> Innskrá </Button>
           </div>
         </form>
         <div className = "register-link">
@@ -28,4 +68,16 @@ class Login extends Component {
 
 /* todo tengja við redux */
 
-export default Login;
+const mapStateToProps = (state) => {
+  console.info(state);
+  
+  return {
+    
+    isFetching: state.auth.isFetching,
+    isAuthenticated: state.auth.isAuthenticated,
+    message: state.auth.message,
+  }
+}
+
+
+export default connect(mapStateToProps)(Login);
