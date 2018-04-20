cce73d737674b6b27158150c4a46575e72cf0c37
Jullira <snulli.julli@gmail.com>
2018-04-20 20:34:22 +0000

    vinna´i alterbooks

--- src/components/alter-book-info/alterBookInfo.js
+++ src/components/alter-book-info/alterBookInfo.js
@@ -11,9 +11,21 @@ import './alterBookInfo.css';
 
 export default class AlterBookInfo extends Component {
 
+  handleInputChange = (e) => {
+    const { name, value } = e.target;
+    if (name) {
+      this.setState({ [name]: value });
+    }
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    const { dispatch } = this.props;
+    dispatch(loginUser(this.state.username, this.state.password));
+  }
 
   render() {
-    const { title, author, isbn13, categorytitle,description, pagecount, language} = this.props.data;
+    const { title, author, isbn13, categorytitle, description, pagecount, language} = this.props.data;
     const { categories } = this.props;
     console.log(categories);
     
@@ -23,26 +35,26 @@ export default class AlterBookInfo extends Component {
           <li>
             <label for = "title">Titill</label>
             <input type = "textbox" name = "title" value = {title}
-            onChange = {(e) => this.setState({ title: e.target.value})} />
+            onChange={this.handleInputChange} />
           </li>
           <li>
             <label for = "author">Höfundur</label>
             <input type = "textbox" name = "author" value={author}
-            onChange = {(e) => this.setState({ author: e.target.value})} />
+            onChange={this.handleInputChange} />
           </li>
           <li>
             <label for = "isbn13">ISBN13</label>
             <input type = "textbox" name ="isbn13" value={isbn13}
-            onChange = {(e) => this.setState({ isbn13: e.target.value})} />
+            onChange = {this.handleInputChange} />
           </li>
           <li>
             <label for = "category">Flokkur</label>
-            <select ref="userInput" defaultvalue="" required>
-              <option value="" disabled>Flokkar</option> {
+            <select ref = "userInput" defaultvalue = "" name = "categorytitle" required>
+              <option value = "categorytitle" disabled>Flokkar</option> {
                 categories.items.map((category) => {
-                  return <option key={category.id}
-                  value={category.title}
-                  onChange = {(e) => this.setState({categorytitle: e.target.value})}
+                  return <option key = {category.id}
+                  value = {category.title}
+                  onChange = {this.handleInputChange}
                   >{category.title}</option>;
                 })
               }
@@ -53,18 +65,18 @@ export default class AlterBookInfo extends Component {
             <textarea name = "description"
              value = {description}
              rows = "10"
-             onChange = {(e) => this.setState({ description: e.target.value})}
+             onChange = {this.handleInputChange}
               />
           </li>
           <li>
             <label for = "pagecount">Blaðsíðufjöldi</label>
             <input type = "textbox" name = "pagecount" value={pagecount}
-            onChange = {(e) => this.setState({ pagecount: e.target.value})} />
+            onChange = {this.handleInputChange} />
           </li>
           <li>
             <label for = "language">Tungumál</label>
             <input type = "textbox" name = "language" value={language}
-            onChange = {(e) => this.setState({ language: e.target.value})} />
+            onChange = {this.handleInputChange} />
           </li>
           <input type = "submit" className="submit" />
 
